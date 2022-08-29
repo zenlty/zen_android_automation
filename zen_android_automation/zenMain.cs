c3347751cf2eef18e7bd83c280c5d882618b69d5
Azat Barış MIZRAKLI <barismizrakli@outlook.com>
2022-08-29 19:40:54 +0300

    Add activate adb root and disable dm verity functions

--- zen_android_automation/zenMain.cs
+++ zen_android_automation/zenMain.cs
@@ -1,31 +1,30 @@
 ﻿using zen_android_automation.Executer;
-
-
+using static zen_android_automation.Enums.Enums;
 
 namespace zen_android_automation
 {
     public class zenAutomation
-    
+
     {
         public string selfDevice { get; set; } = String.Empty;
+        public string initSU { get; set; } = String.Empty;
 
-        
 
         // Initialize class for Executers
         executeCommand executeCommand = new executeCommand();
         // Initialize for Enums/DeviceConfigs
         Enums.Enums deviceConfig = new Enums.Enums();
-        private Enums.Enums.DeviceConfig deviceConfigs;
-
-        public void SetDeviceBuildConfig(Enums.Enums.DeviceConfig value)
+        private Enums.Enums.DeviceConfig initDeviceConfigs;
+        private bool getWaitForExitStatus;
+        public void SetDeviceBuildConfig(DeviceConfig value)
         {
-            deviceConfigs = value;
+            initDeviceConfigs = value;
         }
         public Enums.Enums.DeviceConfig GetDeviceBuildConfig()
         {
-            return deviceConfigs;
+            return initDeviceConfigs;
         }
-        public void zenInit(string? program, string? args, bool waitForExit, bool readOnlyOutput, Enums.Enums.DeviceConfig deviceConfig = Enums.Enums.DeviceConfig.USER, bool usePrebuilt = false)
+        public void zenInit(string? program, string? args, bool waitForExit = true, bool readOnlyOutput = false, DeviceConfig deviceConfig = DeviceConfig.USER, bool usePrebuilt = false)
         {
             if (usePrebuilt)
             {
@@ -39,6 +38,72 @@ namespace zen_android_automation
             executeCommand.waitForExit = waitForExit;
             executeCommand.readOnlyOutput = readOnlyOutput;
             SetDeviceBuildConfig(deviceConfig);
+            InitalizeRootConfigs(deviceConfig);
+
+        }
+        public string ActivateAdbRoot() // Only support USERDEBUG builds
+        {
+            switch (GetDeviceBuildConfig())
+            {
+                case DeviceConfig.USER:
+                    return "Only support USERDEBUG software";
+                case DeviceConfig.USERDEBUG:
+                    getWaitForExitStatus = executeCommand.waitForExit;
+                    Console.WriteLine("Wait for exit set true");
+                    executeCommand.waitForExit = true;
+                    executeCommand.args = "root";
+                    executeCommand.runCommand();
+                    executeCommand.args = "remount";
+                    executeCommand.runCommand();
+                    Console.WriteLine("Wait for exit set : " + getWaitForExitStatus);
+                    return "Executed all process!";
+                    
+                case DeviceConfig.USER_With_Root:
+                    return "Only support USERDEBUG software";
+                default:
+                    return "Please configure init method";
+            }
+        }
+        public string DisableDMVerity() // Only support USERDEBUG builds
+        {
+            switch (GetDeviceBuildConfig())
+            {
+                case DeviceConfig.USER:
+                    return "Only support USERDEBUG software";
+                case DeviceConfig.USERDEBUG:
+                    getWaitForExitStatus = executeCommand.waitForExit;
+                    Console.WriteLine("Wait for exit set true");
+                    executeCommand.waitForExit = true;
+                    executeCommand.args = "root";
+                    executeCommand.runCommand();
+                    executeCommand.args = "remount";
+                    executeCommand.runCommand();
+                    executeCommand.args = "disable-verity";
+                    executeCommand.runCommand();
+                    Console.WriteLine("Wait for exit set : " + getWaitForExitStatus);
+                    Console.WriteLine("Please reboot device and activate root permissions");
+                    return "Executed all process!";
+
+                case DeviceConfig.USER_With_Root:
+                    return "Only support USERDEBUG software";
+                default:
+                    return "Please configure init method";
+            }
+        }
+        public void InitalizeRootConfigs(DeviceConfig deviceConfig)
+        {
+            switch (deviceConfig)
+            {
+                case DeviceConfig.USER:
+                    initSU = "";
+                    break;
+                case DeviceConfig.USERDEBUG:
+                    initSU = "";
+                    break;
+                case DeviceConfig.USER_With_Root:
+                    initSU = " su -c ";
+                    break;
+            }
         }
         public string startServer()
         {
@@ -62,7 +127,6 @@ namespace zen_android_automation
         }
         public string inputText(string text)
         {
-           
             executeCommand.args = "-s " + selfDevice + " shell input text " + text;
             executeCommand.runCommand();
             return "Sent!";
